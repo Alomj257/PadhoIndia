5570100b7aabef3bb8f3cf592e264e647d05fd12
Jahangir Alom <alomj257@gmail.com>
2025-05-01 15:18:31 +0530

    registration done

--- backend-padhoindia/models/User.js
+++ backend-padhoindia/models/User.js
@@ -2,29 +2,50 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema(
-    {
-        firstName: { type: String, required: true },
-        lastName: { type: String, required: true },
-        email: { type: String, required: true, unique: true },
-        password: { type: String, required: true },
-        country: { type: String, required: true },
-        phone: { type: Number, required: true },
-        userType: { 
-            type: String, 
-            required: true, 
-            enum: ["STUDENT", "ADMIN"] 
-        },
-        resetOtp: { type: String },
-        otpExpiry: { type: Date },
+  {
+    // Common Fields
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    country: { type: String, required: true },
+    phone: { type: Number, required: true },
+    dateOfBirth: {
+        type: Date,
+        required: function () {
+          return this.userType === 'STUDENT';
+        }
+      },
+    userType: {
+      type: String,
+      required: true,
+      enum: ["STUDENT", "ADMIN"]
     },
-    { timestamps: true }
+
+    // Student Specific Fields
+    whatsappNumber: { type: Number },
+    address: { type: String },
+    district: { type: String },
+    state: { type: String },
+    pin: { type: String },
+    schoolName: { type: String },
+    studentClass: { type: String},
+    schoolAddress: { type: String },
+    aadharCardNumber: { type: String },
+    registrationNumber: { type: String, unique: true, sparse: true },
+
+    // OTP Reset
+    resetOtp: { type: String },
+    otpExpiry: { type: Date }
+  },
+  { timestamps: true }
 );
 
-// Hash password before saving user
+// Hash password before saving
 userSchema.pre("save", async function (next) {
-    if (!this.isModified("password")) return next();
-    this.password = await bcrypt.hash(this.password, 10);
-    next();
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
 });
 
 module.exports = mongoose.model("User", userSchema);
